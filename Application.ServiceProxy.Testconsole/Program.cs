997cf1cf686e1a21e4f4e66803e59079bec91172
Joydeep Roychowdhury <joydeepbeyondsky86@gmail.com>
2020-12-09 00:47:56 +0530

    feature for dockerization for Application.ServiceProxy

---
+++ Application.ServiceProxy.Testconsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ServiceProxy.Testconsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await JsonTest();
+
+            async Task JsonTest()
+            {
+                List<byte[]> byteList = new List<byte[]>();
+                string json = File.ReadAllText("hextest.json");
+                Console.WriteLine($"JSON  STRING : {json}");
+                byte[] arr = Encoding.UTF8.GetBytes(json);
+                await SendHex(arr);
+            }
+        }
+
+        public static async Task SendHex(byte[] bytes)
+        {
+            UdpClient client = new UdpClient(0);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9012);
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("20.53.116.216"), 9012);
+            int i = 0;
+            while (true)
+            {
+                await client.SendAsync(bytes, bytes.Length, endPoint);
+                Console.WriteLine($"{++i}: message send");
+                //await Task.Delay(2000);
+                break;
+            }
+        }
+    }
+}
---
+++ Application.ServiceProxy/Worker.cs
@@ -0,0 +1,56 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.ServiceProxy
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly UdpClient receivingUdpClient = new UdpClient(9012);
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //Creates an IPEndPoint to record the IP Address and port number of the sender.
+                // The IPEndPoint will allow you to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+
+                    // Blocks until a message returns on this socket from a remote host.
+                    var udpReceiveResult = await receivingUdpClient.ReceiveAsync();
+                    Byte[] receiveBytes = udpReceiveResult.Buffer;
+
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("This is the message you received " +
+                                              returnData.ToString());
+                    Console.WriteLine("This message was sent from " +
+                                                RemoteIpEndPoint.Address.ToString() +
+                                                " on their port number " +
+                                                RemoteIpEndPoint.Port.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
